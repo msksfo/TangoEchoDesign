41bfde1627dbebe9479add8653e6df1152c0110f
Tia <msksfo@gmail.com>
2017-09-15 18:40:56 -0700

    add screen shots of projects with links to github.

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,28 @@
 var contactLink = document.getElementById('contactLink');
 var modal = document.getElementById('modal');
 var close = document.getElementById('close');
+var home = document.getElementById('home');
+var about = document.getElementById('about');
+var portfolio = document.getElementById('portfolio');
 
+var headerHeight = home.clientHeight;
+var portfolioPosition = about.clientHeight;
+
+window.onbeforeunload = function(){
+	window.scrollTo(0, 0);
+}
+
+home.addEventListener('click', function(){
+	scrollTo(document.body, 0, 0);
+});
+
+about.addEventListener('click', function(){
+	scrollTo(document.body, 0, 98);
+});
+
+portfolio.addEventListener('click', function(){
+	scrollTo(document.body, 0, 715);
+});
 
 contactLink.addEventListener('click', function(){
 	modal.classList.add('is-visible');
@@ -10,3 +31,4 @@ contactLink.addEventListener('click', function(){
 close.addEventListener('click', function(){
 	modal.classList.remove('is-visible');
 });
+
