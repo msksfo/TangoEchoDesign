d505afbb57645804346a331d8bd67afad089b8f3
Tia <msksfo@gmail.com>
2017-09-16 06:17:56 -0700

    set max-width on body. delete extra style sheet. delete unused js event listener

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,12 @@
+
 var contactLink = document.getElementById('contactLink');
 var modal = document.getElementById('modal');
 var close = document.getElementById('close');
+
 var home = document.getElementById('home');
 var about = document.getElementById('about');
 var portfolio = document.getElementById('portfolio');
 
-var headerHeight = home.clientHeight;
-var portfolioPosition = about.clientHeight;
 
 window.onbeforeunload = function(){
 	window.scrollTo(0, 0);
@@ -18,10 +18,6 @@ home.addEventListener('click', function(){
 
 about.addEventListener('click', function(){
 	scrollTo(document.body, 0, 98);
-});
-
-portfolio.addEventListener('click', function(){
-	scrollTo(document.body, 0, 715);
 });
 
 contactLink.addEventListener('click', function(){
